2e85f7f29c708be38c22d1172fbf8bd7c06d4d29
shashank <shashank@maker.co>
2023-10-12 23:49:32 +0530

    stable

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,12 +18,30 @@ const config: PlaywrightTestConfig = {
     open: "on-failure"
   }]],
   use: {
-    headless: process.env.CI ? true : false,
-    baseURL: "https://bookcart.azurewebsites.net/",
-    // actionTimeout: 2 * 60 * 1000,
-    trace: process.env.CI ? "off" : "on",
-    video: process.env.CI ? "off" : "on",
-    screenshot: process.env.CI ? "off" : "on",
+    ...devices[process.env.DEVICE_NAME || 'Desktop Chrome'],
+    // viewport: { width: 1200, height: 800 },
+    launchOptions: {
+      args: ["--start-maximized"],
+      slowMo: process.env.SLOW_MO ? parseInt(process.env.SLOW_MO) : 0,
+    },
+    screenshot: process.env.ENVIRONMENT_NAME === 'preview' || process.env.ENVIRONMENT_NAME === 'production'
+      ? 'only-on-failure'
+      : 'only-on-failure',
+
+    headless: true,
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 15000,
+    navigationTimeout: 15000,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on',
+    video: {
+      mode: 'on',
+      size: { width: 1100, height: 800 }
+    },
+
   },
   projects: [
     {
