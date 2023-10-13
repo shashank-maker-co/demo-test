35475de667646d71825b1747d1c8d6e0072eb4f6
shashank <shashank@maker.co>
2023-10-13 11:08:42 +0530

    stable-3 adding git metadata

--- checkly.config.ts
+++ checkly.config.ts
@@ -1,6 +1,37 @@
 import { defineConfig } from 'checkly'
 
-export default defineConfig({
+interface GitConfig {
+    branch: string;
+    commitSha: string;
+    owner: string;
+}
+
+interface ChecklyConfig {
+    projectName: string;
+    logicalId: string;
+    repoUrl: string;
+    checks: {
+        activated: true;
+        muted: false;
+        runtimeId: string;
+        frequency: number;
+        locations: ("us-east-1" | "eu-west-1")[];
+        tags: string[];
+        alertChannels: never[];
+        checkMatch: string;
+        ignoreDirectoriesMatch: never[];
+        browserChecks: {
+            frequency: number;
+            testMatch: string;
+        };
+    };
+    cli: {
+        runLocation: string;
+    };
+    git: GitConfig;
+}
+
+export default defineConfig<ChecklyConfig>({
     projectName: 'Playwright Checks Pages',
     logicalId: 'website-monitoring-1',
     repoUrl: 'https://github.com/shashank-maker-co/checks-playwright',
@@ -21,5 +52,10 @@ export default defineConfig({
     },
     cli: {
         runLocation: 'eu-west-1',
+    },
+    git: {
+        branch: "${CHECKLY_GIT_BRANCH}",
+        commitSha: "${CHECKLY_GIT_COMMIT_SHA}",
+        owner: "${CHECKLY_GIT_OWNER}"
     }
 })
