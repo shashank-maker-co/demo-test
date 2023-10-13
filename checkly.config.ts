57eb1b8cb5551e3a854b864a9b885a3f42cada3a
shashank <shashank@maker.co>
2023-10-13 11:35:43 +0530

    stable-3 adding git metadata-2

--- checkly.config.ts
+++ checkly.config.ts
@@ -1,40 +1,9 @@
 import { defineConfig } from 'checkly'
-
-interface GitConfig {
-    branch: string;
-    commitSha: string;
-    owner: string;
-}
-
-interface ChecklyConfig {
-    projectName: string;
-    logicalId: string;
-    repoUrl: string;
-    checks: {
-        activated: true;
-        muted: false;
-        runtimeId: string;
-        frequency: number;
-        locations: ("us-east-1" | "eu-west-1")[];
-        tags: string[];
-        alertChannels: never[];
-        checkMatch: string;
-        ignoreDirectoriesMatch: never[];
-        browserChecks: {
-            frequency: number;
-            testMatch: string;
-        };
-    };
-    cli: {
-        runLocation: string;
-    };
-    git: GitConfig;
-}
-
-export default defineConfig<ChecklyConfig>({
-    projectName: 'Playwright Checks Pages',
+import { env } from './env'
+export default defineConfig({
+    projectName: 'checks playwright',
     logicalId: 'website-monitoring-1',
-    repoUrl: 'https://github.com/shashank-maker-co/checks-playwright',
+    repoUrl: process.env.CIRCLE_BUILD_URL || 'https://github.com/shashank-maker-co/checks-playwright',
     checks: {
         activated: true,
         muted: false,
@@ -53,9 +22,4 @@ export default defineConfig<ChecklyConfig>({
     cli: {
         runLocation: 'eu-west-1',
     },
-    git: {
-        branch: "${CHECKLY_GIT_BRANCH}",
-        commitSha: "${CHECKLY_GIT_COMMIT_SHA}",
-        owner: "${CHECKLY_GIT_OWNER}"
-    }
 })
